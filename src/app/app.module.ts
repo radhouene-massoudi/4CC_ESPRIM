c44a5e107ab0b7aaec34f85f3a2857ecfa76dba0
massoudiradhouene <radhouane.massoudi@esprit.tn>
2026-01-28 22:49:42 +0100

    seance 2 &&3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FirstComponent } from './first/first.component';
+import { TestComponent } from './test/test.component';
+import { FormsModule } from '@angular/forms';
+import { CouldComponent } from './could/could.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    TestComponent,
+    CouldComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
